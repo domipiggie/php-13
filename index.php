096c417dc5e88f0fcdf9715fe8c4cbc088058590
domipiggie <domipiggie@gmail.com>
2024-11-04 08:14:11 +0100

    valami

--- index.php
+++ index.php
@@ -1,65 +1,156 @@
 <?php
-$ulesrend = [
-    ["1.oszlop", "2.oszlop", "Folyosó", "3.oszlop", "4.oszlop", "Folyosó", "5.oszlop"],
-    ["-", "Tanár",  "Gulyás Máté", "Lénárt Áron",  "-"],
-    ["Mészáros Marcell", "Básti Domonkos",  "Keindl Bercel", "Kis Balázs",  "-"],
-    ["Csik Melinda", "Karakas Roland",  "Ábrahám Dávid", "Détári Leon",  "Togyeriska Viktor"],
-    ["-", "-",  "Giczi Atilla", "Preil Ákos",  "Sivinger Miklós"]
-];
-?>
 
+$servername = "localhost";
+$username = "teszt";
+$password = "teszt";
+$dbname = "php_teszt";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+mysqli_set_charset($conn, 'utf8');
+
+// Check connection
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+}
+
+$name = '';
+
+if ($_SERVER["REQUEST_METHOD"] == "POST"){
+    if (empty($_POST["keresett_nev"])){
+        $nameErr = "Nem írtál be nevet 💀";
+    } else if (strlen($_POST["keresett_nev"]) < 2){
+        $nameErr = "Írj be legalább két karaktert";
+    } else {
+        $name = $_POST["keresett_nev"];
+    }
+}
+
+?>
 <!DOCTYPE html>
 <html lang="hu">
-
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     <title>Document</title>
-    <style>
-        td,
-        th {
-            border: 1px solid black;
+</head>
+<body>
+    <div>
+        <h1> 13. I. 1. csoport</h1>
+        <h2> Ülésrend</h2>
+    </div>
+    <?php if(isset($nameErr)) echo $nameErr; ?>
+    <form action="index.php" method="POST">
+        <input type="text" name="keresett_nev" value="<?php echo $name; ?>">
+        <button type="submit">Keresés</button>
+    </form>
+    <table class="table table-striped">
+        <thead>
+          <tr>
+            <th scope="col">#</th>
+            <th scope="col">Első Oszlop</th>
+            <th scope="col">Második Oszlop</th>
+            <th scope="col">Folyosó</th>
+            <th scope="col">Harmadik Oszlop </th>
+            <th scope="col">Negyedik Oszlop</th>
+            <th scope="col">Folyosó</th>
+            <th scope="col">Ötödik Oszlop</th>
+          </tr>
+        </thead>
+        <tbody>
+          <?php
+
+            // adatbázis alapú adatok
+            $sql = "SELECT id, nev, sor, oszlop FROM osztaly ORDER BY sor, oszlop";
+            $result = $conn->query($sql);
+
+            $sor = NULL;
+            
+            if ($result->num_rows > 0) {
+              // output data of each row
+              while($row = $result->fetch_assoc()) {
+                if($sor !== $row["sor"]) {
+                  if($sor !== NULL) echo "</tr>";
+                  ?>
+                  <tr>
+                  <th scope="row"><?php echo $row["sor"] + 1; ?></th>
+                  <?php
+                  $sor = $row["sor"];
+                }
+                $class = '';
+                if ($name != '' && stripos($row['nev'],$name) !== FALSE){
+                    echo "<td style='background-color:red; color:white;'>" . $row["nev"]. "</td>";
+                } else {
+                    echo "<td>" . $row["nev"]. "</td>";
+                }
+                if($row["oszlop"] == 1 or $row["oszlop"] == 3) {
+                  echo "<td>  </td>";
+                }
+                // echo "id: " . $row["id"]. " - Name: " . $row["nev"]. " " . $row["sor"]. " " . $row["oszlop"]. "<br>";
+              }
+            } 
+            else {
+              echo "0 results";
+            }
+
+
+          ?>
+          </tr>
+        </tbody>
+      </table>
+
+      <style>
+        body{
+            padding: 60px;
+            text-align: center;
+            background-color: rgb(236, 232, 227);
+        }
+
+        h1{
+            padding: 20px auto;
+            text-align: center;
+        }
+
+        h2{
+            padding: 20px auto;
+            text-align: center;
+            margin-bottom: 40px;
+        }
+        label,input{
+            display: block;
+        }
+        label{
             width: 100%;
-            padding: 10px;
         }
-
-        tr {
-            display: flex;
-            justify-content: space-between;
-        }
-
-        table {
-            width: 90%;
+        form{
+            width: 40%;
             margin-left: auto;
             margin-right: auto;
+            display: flex;
+            border-radius: 10px;
+            margin-bottom: 20px;
         }
-
-        h1 {
-            text-align: center;
-            width: 100%;
+        input{
+            padding: 5px;
+            margin-bottom: 10px;
+            width: calc(100% - 20px);
+            border-radius: 5px;
+            border: 1px solid black;
         }
-    </style>
-</head>
-
-<body>
-    <h1>13.i Ülésrend</h1>
-    <table>
-        <?php
-        foreach ($ulesrend as $i => $sor) {
-            $offset = 0;
-            echo "<tr>";
-            echo $i != 0 ? "<th>$i</th>" : "<th>#</th>";
-            foreach ($sor as $j=>$diak) {
-                if ($ulesrend[0][$j+$offset]=="Folyosó" and $i>0){
-                    echo "<td></td>";
-                    $offset++;
-                }
-                echo $i == 0 ? "<th>$diak</th>" : "<td>$diak</td>";
-            }
-            echo "</tr>";
+        button{
+            padding: 5px;
+            width: 25%;
+            background-color: #1bace7;
+            border: none;
+            border-radius: 5px;
         }
-        ?>
-    </table>
+      </style>
+    
 </body>
-
 </html>
+<?php
+
+$conn->close(); 
+
+?>
